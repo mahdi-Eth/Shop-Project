3975fd2282a4b90e7e53a1b0f5386ddc05fd44f8
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2022-09-23 07:17:50 +0330

    friday commit

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import ImageLoader from 'image-loader';
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const merge = require("webpack-merge");
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-  devtool: 'inline-source-map',
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Development',
-     }),
-   ],
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: ["img-loader"],
+      },
+    ],
+  },
+};
+
+module.exports = config;
